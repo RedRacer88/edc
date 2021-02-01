7de8adc982bce5557b1913b16633b4d70dd436bb
RedRacer88 <daymen88@gmail.com>
2021-01-31 21:39:09 -0800

    Initial Commit

---
+++ src/stocks/stockController.ts
@@ -0,0 +1,7 @@
+/*
+const alpha = require("alphavantage")({ key: process.env.ALPHAVANTAGE_KEY })
+alpha.data.intraday('msft').then((data: string) => {
+    const polished = alpha.util.polish(data)
+    console.log(polished)
+})
+*/
